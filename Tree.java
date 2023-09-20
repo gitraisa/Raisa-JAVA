dba4c11dfced197d102451ca847da3f73a64aa6e
Raisa Tasnim <113124038+gitraisa@users.noreply.github.com>
2023-09-20 15:02:30 -0500

    Create Tree.java

---
+++ Tree.java
@@ -0,0 +1,15 @@
+// Define your tree class in this file!
+public class Tree {
+  public static void main(String[] args) {
+  //We're going to introduce outselves and print a tree to the screen.
+  System.out.println("Hello! My name is Raisa and I'm learning to code in Java!'");
+  System.out.println("I am going to plant a tree today! I'm ready to get my hands dirty!'");
+  System.out.println("  *  ");
+  System.out.println(" *** ");
+  System.out.println("*****");
+  System.out.println(" *** ");
+  System.out.println("  *  ");
+  System.out.println("  *  ");
+  }
+
+}
